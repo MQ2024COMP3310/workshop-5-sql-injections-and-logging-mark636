4b9524179aaec4b516f6d67f03eecd46fcf55b48
mark636 <mark.mathew@students.mq.edu.au>
2024-03-21 11:44:35 +1100

    updated input function

--- app/src/main/java/workshop05code/App.java
+++ app/src/main/java/workshop05code/App.java
@@ -72,18 +72,18 @@ public class App {
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter a 4 letter word for a guess or q to quit: ");
             String guess = scanner.nextLine();
-
             while (!guess.equals("q")) {
                 System.out.println("You've guessed '" + guess+"'.");
-
-                if (wordleDatabaseConnection.isValidWord(guess)) { 
+                 
+                if (wordleDatabaseConnection.isValidWord(guess)&& guess.matches("[a-z]{4}")) { 
                     System.out.println("Success! It is in the the list.\n");
                 }else{
-                    System.out.println("Sorry. This word is NOT in the the list.\n");
+                    System.out.println("Sorry. This word is NOT in the the list or not valid\n");
                 }
 
                 System.out.print("Enter a 4 letter word for a guess or q to quit: " );
                 guess = scanner.nextLine();
+               
             }
         } catch (NoSuchElementException | IllegalStateException e) {
             e.printStackTrace();
